1e3a0a9e872b9af74924ab9ff7367c1d143dbc98
xzh263 <47003054+xzh263@users.noreply.github.com>
2021-08-12 14:53:28 -0400

    Update SF_CMP2021_Auto_LOS_Reliability.py
    Updates made include:
    (1) increase the sample size for reliability calculation by relaxing spatial coverage requirement; and 
    (2) calculate the 50th percentile speed and LOTTR reliability measure

--- INRIX/SF_CMP2021_Auto_LOS_Reliability.py
+++ INRIX/SF_CMP2021_Auto_LOS_Reliability.py
@@ -21,6 +21,9 @@ INPUT_PATHS = [['SF_county_network_for_CMP2021_2021-04-05_to_2021-05-22_1_min_pa
 # Minimum sample size for the AM/PM monitoring period
 ss_threshold = 180
 
+# Minimum spatial coverage for reliability measurement
+r_spatial_thrd= 70  # to be consistent with the minimum value used in LOS calculation
+
 # Input CMP segment shapefile
 cmp_segs=gp.read_file(os.path.join(r'Q:\CMP\LOS Monitoring 2021\CMP_plus_shp', 'cmp_segments_plus.shp'))
 
@@ -64,7 +67,7 @@ fcr['cmp_segid'] = fcr['cmp_segid'].astype(int)
 fcr = pd.merge(fcr, cmp_segs[['cmp_segid', 'length']], on='cmp_segid', how='left')
 fcr['speed'] = round(3600*fcr['length']/fcr['traveltime'], 3)
 fcr_avg_spds = fcr.groupby(['cmp_segid', 'period']).agg({'speed': ['mean', 'std', 'min', 'max', 'count']}).reset_index()
-fcr_avg_spds.columns = ['cmp_segid', 'period', 'avg_speed', 'std_speed', 'min_speed', 'max_speed', 'sample_size']
+fcr_avg_spds.columns = ['cmp_segid', 'period', 'avg_speed', 'std_speed', 'min_speed', 'max_speed', 'sample_size_los']
 
 #Save the floating run average speed
 fcr_avg_spds.to_csv(FCR_DIR + '\floating_car_average_speeds.csv', index=False)
@@ -238,21 +241,21 @@ def spatial_coverage(df, cmp_period_agg, group_cols, rename_cols, coverage_thres
     cmp_tt_agg = df[(~df['CMP_SegID'].isin(cmp_period_agg['cmp_segid'])) & 
                     (df['SpatialCov']>=coverage_threshold)].groupby(group_cols).agg({'TT': ['count', 'sum'],
                                                                                      'Length_Matched': 'sum',
-                                                                                     'Speed': ['std', percentile(5), percentile(20)]}).reset_index()
+                                                                                     'Speed': ['std']}).reset_index()
     cmp_tt_agg.columns = rename_cols
     cmp_tt_agg['avg_speed'] = round(cmp_tt_agg['Len']/cmp_tt_agg['TT'],3)
     cmp_tt_agg['cov'] = round(100*cmp_tt_agg['std_speed']/cmp_tt_agg['avg_speed'],3)
     cmp_tt_agg = cmp_tt_agg[cmp_tt_agg['sample_size']>=ss_threshold]
     
     if len(cmp_tt_agg)>0:
-        cmp_tt_agg['comment'] = 'Calculation performed on ' + str(coverage_threshold) +'% or greater of length'
+        cmp_tt_agg['comment'] = 'LOS calculation performed on ' + str(coverage_threshold) +'% or greater of length'
         cmp_period_agg = cmp_period_agg.append(cmp_tt_agg, ignore_index=True)
         
     return cmp_period_agg
 
 
 # Calculate CMP segment level average speeds, LOS, and reliability metrics
-def cmp_seg_level_speed_and_los(df_cmp_period, ss_threshold, floating_car_spds, cur_year, cur_period):
+def cmp_seg_level_speed_and_los(df_cmp_period, ss_threshold, r_spatial_thrd, floating_car_spds, cur_year, cur_period):
     df_cmp_period['TT'] = df_cmp_period['Length_Matched']/df_cmp_period['Speed(miles/hour)']
 
     # Get total travel time at a particular date_time on a CMP segment
@@ -264,9 +267,15 @@ def cmp_seg_level_speed_and_los(df_cmp_period, ss_threshold, floating_car_spds, 
     cmp_period['SpatialCov'] = 100*cmp_period['Length_Matched']/cmp_period['CMP_Length']  #Spatial coverage 
     
     group_cols = ['CMP_SegID']
-    rename_cols = ['cmp_segid', 'sample_size', 'TT', 'Len', 'std_speed', 'pcnt5th', 'pcnt20th']
+    rename_cols = ['cmp_segid', 'sample_size_los', 'TT', 'Len', 'std_speed']
     cmp_period_agg = pd.DataFrame(columns = rename_cols)
     
+    # Use minimum 70% spatial coverage for reliability metric calculation
+    cmp_period_r = cmp_period[cmp_period['SpatialCov']>=r_spatial_thrd].groupby(group_cols).agg({'Speed': ['count', percentile(5), percentile(20), percentile(50)]}).reset_index()
+    cmp_period_r.columns = ['cmp_segid', 'sample_size_rel', 'pcnt5th', 'pcnt20th', 'pcnt50th']
+    cmp_period_r = cmp_period_r[cmp_period_r['sample_size_rel']>=ss_threshold]
+    
+    # Calculate avg and std of speeds based on different spatial coverage requirements
     # 99% spatial coverage 
     cmp_period_agg = spatial_coverage(cmp_period, cmp_period_agg, group_cols, rename_cols, coverage_threshold = 99)
     
@@ -299,24 +308,25 @@ def cmp_seg_level_speed_and_los(df_cmp_period, ss_threshold, floating_car_spds, 
     cmp_period_frc['year'] = cur_year
     cmp_period_frc['source'] = 'Floating Car'
 
-    cmp_period_combine = cmp_period_agg.append(cmp_period_frc[['cmp_segid', 'year', 'source', 'period', 'avg_speed', 'std_speed', 'sample_size']], ignore_index=True)
+    cmp_period_combine = cmp_period_agg.append(cmp_period_frc[['cmp_segid', 'year', 'source', 'period', 'avg_speed', 'std_speed', 'sample_size_los']], ignore_index=True)
     
     cmp_period_combine = pd.merge(cmp_period_combine, cmp_segs[['cmp_segid', 'cls_hcm85', 'cls_hcm00']], on='cmp_segid', how='left')
     cmp_period_combine['los_hcm85'] = cmp_period_combine.apply(lambda x: los_1985(x.cls_hcm85, x.avg_speed), axis=1)
     cmp_period_combine['los_hcm00'] = cmp_period_combine.apply(lambda x: los_2000(x.cls_hcm00, x.avg_speed), axis=1)
 
-    cmp_period_combine = cmp_period_combine[['cmp_segid', 'year', 'source', 'period', 'avg_speed', 'los_hcm85', 'los_hcm00', 'std_speed', 'pcnt5th', 'pcnt20th', 'cov', 'sample_size', 'comment']]
+    cmp_period_combine = cmp_period_combine[['cmp_segid', 'year', 'source', 'period', 'avg_speed', 'los_hcm85', 'los_hcm00', 'sample_size_los', 'comment', 'std_speed', 'cov', 'pcnt5th', 'pcnt20th', 'pcnt50th', 'sample_size_rel']]
     
     return cmp_period_combine
     
 
-cmp_am_agg = cmp_seg_level_speed_and_los(df_cmp_am, ss_threshold, fcr_avg_spds, cur_year = 2021, cur_period = 'AM')
-cmp_pm_agg = cmp_seg_level_speed_and_los(df_cmp_pm, ss_threshold, fcr_avg_spds, cur_year = 2021, cur_period = 'PM')
+cmp_am_agg = cmp_seg_level_speed_and_los(df_cmp_am, ss_threshold, r_spatial_thrd, fcr_avg_spds, cur_year = 2021, cur_period = 'AM')
+cmp_pm_agg = cmp_seg_level_speed_and_los(df_cmp_pm, ss_threshold, r_spatial_thrd, fcr_avg_spds, cur_year = 2021, cur_period = 'PM')
 
 cmp_segs_los = cmp_am_agg.append(cmp_pm_agg, ignore_index=True)
 cmp_segs_los = pd.merge(cmp_segs_los, cmp_segs_refspd[['cmp_segid', 'refspd_inrix']], on='cmp_segid', how='left')
-cmp_segs_los['tti95'] = np.maximum(1, cmp_segs_los['refspd_inrix']/cmp_segs_los['pcnt5th'])
-cmp_segs_los['tti80'] = np.maximum(1, cmp_segs_los['refspd_inrix']/cmp_segs_los['pcnt20th'])
-cmp_segs_los['bi'] = np.maximum(0, cmp_segs_los['avg_speed']/cmp_segs_los['pcnt5th']-1)
+cmp_segs_los['tti95'] = np.maximum(1, round(cmp_segs_los['refspd_inrix']/cmp_segs_los['pcnt5th'], 3))
+cmp_segs_los['tti80'] = np.maximum(1, round(cmp_segs_los['refspd_inrix']/cmp_segs_los['pcnt20th'], 3))
+cmp_segs_los['bi'] = np.maximum(0, round(cmp_segs_los['avg_speed']/cmp_segs_los['pcnt5th']-1, 3))
+cmp_segs_los['lottr'] = np.maximum(1, round(cmp_segs_los['pcnt50th']/cmp_segs_los['pcnt20th'], 3))
 
 cmp_segs_los.to_csv(os.path.join(OUT_DIR, OUT_FILE), index=False)
