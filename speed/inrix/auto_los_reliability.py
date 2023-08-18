14be292c2d323d5208a6a0e0d1cb1f26912966f5
Chun Ho Chow <15370962+chunhochow@users.noreply.github.com>
2023-08-17 17:04:03 -0700

    auto_los_reliability.py: hardcoded inputs -> toml

--- speed/inrix/auto_los_reliability.py
+++ speed/inrix/auto_los_reliability.py
@@ -7,24 +7,13 @@ from core import (
 )
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("toml_filepath")
-    # args = parser.parse_args()
-    # with open(args.toml_filepath, "rb") as f:
-    #     config = tomllib.load(f)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("toml_filepath")
+    args = parser.parse_args()
+    with open(args.toml_filepath, "rb") as f:
+        config = tomllib.load(f)
 
-    network_conflation_correspondence_filepath = r"Q:\CMP\LOS Monitoring 2021\Network_Conflation\CMP_Segment_INRIX_Links_Correspondence_2101_Manual_PLUS_Updated.csv"
-    DATA_DIR = r"Q:\Data\Observed\Streets\INRIX\v2101"
-    out_filepath_stem = r"Q:\CMP\LOS Monitoring 2023\Auto_LOS_and_Reliability\CMP2021_Auto_LOS_and_Reliability"
-    INPUT_GLOB = "SF_county_network_for_CMP2021_2021-04-05_to_2021-05-22_1_min_part_*.zip"
-    cmp_segments_gis_filepath = r"Q:\CMP\LOS Monitoring 2021\CMP_plus_shp\old_cmp_plus\cmp_segments_plus.shp"
-    floating_car_run_speeds_filepath = r"Q:\Data\Observed\Streets\Speed\CMP-floating_car_run\2021\floating_car-speed-summary_stats.csv"
-    year = 2021
-
-    peak_min_sample_size_threshold = (
-        180  # for the AM/PM peak monitoring period
-    )
-
+    peak_min_sample_size_threshold = 180  # AM/PM peak monitoring period
     spatial_coverage_thresholds = [99, 95, 90, 85, 80, 75, 70]
     # Minimum spatial coverage for reliability measurement
     reliability_spatial_coverage_threshold = 70
@@ -37,18 +26,22 @@ if __name__ == "__main__":
         )
 
     # Read in the INRIX data
-    zip_filepaths = glob_inrix_xd_zips_directory(DATA_DIR, INPUT_GLOB)
+    zip_filepaths = glob_inrix_xd_zips_directory(
+        config["input"]["dir"], config["input"]["glob"]
+    )
 
     read_and_process_speed_to_los_and_reliability(
         zip_filepaths,
-        out_filepath_stem,
-        network_conflation_correspondence_filepath,
-        cmp_segments_gis_filepath,
+        config["output"]["filepath_stem"],
+        config["geo"]["network_conflation_correspondence_filepath"],
+        config["geo"]["cmp_segments_gis_filepath"],
         spatial_coverage_thresholds,
-        year,
+        config["input"]["year"],
         peak_min_sample_size_threshold,
         monthly_update=False,
-        floating_car_run_speeds_filepath=floating_car_run_speeds_filepath,
+        floating_car_run_speeds_filepath=config["input"][
+            "floating_car_run_speeds_filepath"
+        ],
         hourly_min_sample_size_threshold=None,
         reliability_spatial_coverage_threshold=reliability_spatial_coverage_threshold,
     )
