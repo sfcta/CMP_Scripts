47d1fab23f44f023e3f03c82ca4f67720509a4ea
Chun Ho Chow <15370962+chunhochow@users.noreply.github.com>
2023-10-24 10:05:40 -0700

    speed/inrix/xd_segments/base: os.path -> pathlib, update zip filepath for 2302

--- speed/inrix/xd_segments/base.py
+++ speed/inrix/xd_segments/base.py
@@ -1,10 +1,11 @@
 """Tools to manipulate INRIX XD road network segments"""
+from pathlib import Path
+
 import geopandas as gpd
-from os.path import join
 
 
-def _map_dir(map_release_version: str) -> str:
-    return join("Q:\GIS\Transportation\Roads\INRIX\XD", map_release_version)
+def _map_dir(map_release_version: str) -> Path:
+    return Path("Q:\GIS\Transportation\Roads\INRIX\XD", map_release_version)
 
 
 def _sf_gis_filename(map_release_version: str) -> str:
@@ -12,8 +13,8 @@ def _sf_gis_filename(map_release_version: str) -> str:
     return f"INRIX_XD-SF-{map_release_version}.gpkg"
 
 
-def sf_gis_filepath(map_release_version: str) -> str:
-    return join(
+def sf_gis_filepath(map_release_version: str) -> Path:
+    return Path(
         _map_dir(map_release_version), _sf_gis_filename(map_release_version)
     )
 
@@ -31,12 +32,12 @@ def read_bayarea_xd_segments(
     map_dir = _map_dir(map_release_version)
     # as downloaded from INRIX, without .shp or .zip:
     bayarea_gis_filename_noext = "USA_CA_BayArea_shapefile"
-    filepath = join(
+    filepath = Path(
         map_dir,
         "maprelease-shapefiles",
         f"{bayarea_gis_filename_noext}.zip",
     )
-    if map_release_version >= "2301":
+    if map_release_version == "2301":
         gdf = gpd.read_file(f"zip://{filepath}!{bayarea_gis_filename_noext}")
     else:
         gdf = gpd.read_file(f"zip://{filepath}")
