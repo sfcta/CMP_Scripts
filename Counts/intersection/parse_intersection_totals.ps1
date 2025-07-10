458affdfb8644398c0206aec8315fe0083c9f423
CHOW Chun Ho <chunhochow@users.noreply.github.com>
2025-07-10 15:31:04 -0700

    Counts/intersection/ parse_intersection_totals.ps1:  add 2025 compare_years.ipynb: update for 2025, fix polars melt -> unpivot, uncomment diff stddev heatmap

--- Counts/intersection/parse_intersection_totals.ps1
+++ Counts/intersection/parse_intersection_totals.ps1
@@ -1,8 +1,12 @@
-# python parse_intersection_totals.py `
+# uv run parse_intersection_totals.py `
 #   "Q:\CMP\LOS Monitoring 2021\Counts\Intersection\2021 SFCTA AM TMC Count Results.xlsx" `
 #   "Q:\CMP\LOS Monitoring 2021\Counts\Intersection\2021 SFCTA PM TMC Count Results.xlsx" `
 #   "Q:\Data\Observed\Streets\Counts\CMP\2021\intersection\parsed-intersection-totals.csv"
-# python parse_intersection_totals.py `
+# uv run parse_intersection_totals.py `
 #   "Q:\Data\Observed\Streets\Counts\CMP\2023\intersection\2023 SFCTA AM TMC Count Results (SFCTA) April May 2023.xlsx" `
 #   "Q:\Data\Observed\Streets\Counts\CMP\2023\intersection\2023 SFCTA PM TMC Count Results (SFCTA) April May 2023.xlsx" `
 #   "Q:\Data\Observed\Streets\Counts\CMP\2023\intersection\parsed-intersection-totals.csv"
+# uv run parse_intersection_totals.py `
+#   "Q:\Data\Observed\Streets\Counts\CMP\2025\intersection\2025 SFCTA CMP - AM TMC Results.xlsx" `
+#   "Q:\Data\Observed\Streets\Counts\CMP\2025\intersection\2025 SFCTA CMP - PM TMC Results.xlsx" `
+#   "Q:\Data\Observed\Streets\Counts\CMP\2025\intersection\parsed-intersection-totals.csv"
